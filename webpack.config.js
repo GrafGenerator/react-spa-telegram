5b65480a9d5083899f3321c2792a3824c70acca0
Nikita Ivanov <grafgenerator@gmail.com>
2018-03-31 14:54:51 +0700

    adapt for hgithub pages

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = (env) => {
             },
             output: {
                 filename: "[name].js",
-                publicPath: "/",
+                publicPath: "/react-spa-telegram",
                 path: "dist",
             },
             devServer: {
